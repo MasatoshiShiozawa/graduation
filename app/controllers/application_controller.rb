b02b8381d58c7390a65d8da427cc32e57c01c9f2
MasatoshiShiozawa <victorynov21@gmail.com>
2021-02-09 10:15:41 +0000

    0209修正2回目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
-  unless Rails.env.production?
+  unless Rails.env.production?, Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
